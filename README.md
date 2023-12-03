9a58e212b748bbba5a1fe6f855e50aa1b9c40adf
ganeshadhikari@bajratechnologies.com <ganeshadhikari@bajratechnologies.com>
2023-12-03 16:37:24 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+1. React jsx element
+<>
+<h1> Hello to react</h1>
+</>
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    /**
+     * <> React  fragment
+     * </>
+     * React jsx element
+     */
+    <>
+      <h1>Hello to basic react</h1>
+      <div style={{ height: '300px', border: '1px solid green' }}>
+      </div>
+    </>
+  )
+}
+
+export default App
