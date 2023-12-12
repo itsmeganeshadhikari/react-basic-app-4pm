b880eb285f877d2409f0ea5bbce09cada057142e
itsmeganeshadhikari <itsmeganeshadhikari@gmail.com>
2023-12-12 11:16:23 +0545

    Demo todo

---
+++ src/components/DemoTodo.jsx
@@ -0,0 +1,10 @@
+import InputBasic from "./InputBasic"
+
+const DemoTodo = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '40px' }}>
+            <InputBasic />
+        </div>
+    )
+}
+export default DemoTodo
