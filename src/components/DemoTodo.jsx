f751379f042689c5a3a1a55c1608608d2bec605b
itsmeganeshadhikari <itsmeganeshadhikari@gmail.com>
2023-12-13 16:09:35 +0545

    Update into  input feild

--- src/components/DemoTodo.jsx
+++ src/components/DemoTodo.jsx
@@ -3,7 +3,7 @@ import InputBasic from "./InputBasic"
 const DemoTodo = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '40px' }}>
-            <InputBasic />
+            {/* <InputBasic /> */}
         </div>
     )
 }
--- src/components/InputBasic.jsx
+++ src/components/InputBasic.jsx
@@ -2,30 +2,30 @@ import { Box, Button, TextField, Typography } from "@mui/material"
 import AddIcon from '@mui/icons-material/Add';
 import { useState } from "react";
 import { Delete } from "@mui/icons-material";
+
+const inital = [{ name: "Ram" }]
 const InputBasic = () => {
-    const [value, setValue] = useState({ name: "" });
-    const [name, setName] = useState({ name: "" });
+    const [value, setValue] = useState();
+    const [name, setName] = useState(inital);
+
     const addItem = (e) => {
         e.preventDefault()
-        setValue({
-            [name]: name
-        });
     }
     console.log(value);
 
     return (
         <div flexDirection="column">
             <Box display='flex' padding={2}>
-                <TextField sx={{ width: '500px' }} id="outlined-basic" label="Enter a fruits" variant="outlined" name="name" value={name.name} onChange={(e) => {
-                    setName({ ...name, [e.target.name]: e.target.value })
-                }} />
+                <TextField sx={{ width: '500px' }} id="outlined-basic" label="Enter a fruits" variant="outlined" name="name" value={ } />
                 <Button sx={{ marginLeft: '5px' }} variant="contained" startIcon={<AddIcon />} onClick={addItem} >Add</Button>
             </Box>
             <Box display="flex" padding={3}>
-                {/* {value.map((e) => { */}
-                <Typography color="blue" marginTop={1}></Typography>
-                {/* })} */}
-                <Button variant="text" startIcon={<Delete sx={{ color: 'red' }} />} />
+                {inital.map((e) => {
+                    return (
+                        <Typography color="blue" marginTop={1}>{e.name}
+                            <Button variant="text" startIcon={<Delete sx={{ color: 'red' }} />} />
+                        </Typography>)
+                })}
             </Box>
         </div >
     )
