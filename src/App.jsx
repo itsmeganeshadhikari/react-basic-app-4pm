d6a5f971a23bf1da3e9d5eac5011d1f2e44a5846
itsmeganeshadhikari <itsmeganeshadhikari@gmail.com>
2023-12-13 16:53:36 +0545

    Changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import DemoTodo from './components/DemoTodo'
+// import DemoTodo from './components/DemoTodo'
+
+import { createContext } from "react"
+import PropsDemo from "./components/PropsDemo"
+
+export const UserContext = createContext()
 
 function App() {
+  const lastName = "Sharama";
+
   return (
     <>
-      <DemoTodo />
+      {/* <h1>Hello</h1> */}
+      {/* <DemoUseEffectHook /> */}
+      <UserContext.Provider value={lastName}>
+        <PropsDemo />
+      </UserContext.Provider>
     </>
   )
 }
